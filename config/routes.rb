7a31bd9000f4503b99693c9a58b175d381a6554a
Jason Ellis <palamedes@rocketmail.com>
2020-10-07 16:34:41 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/progress_notes/:therapist_id/:client_id/notes/', to: "notes#index", as: :notes
+  get '/progress_notes/:therapist_id/:client_id/notes/:id/edit', to: "notes#edit", as: :edit_note
+
+  # Default index route
+  root 'application#index'
+
 end
