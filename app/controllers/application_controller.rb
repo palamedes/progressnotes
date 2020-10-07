c513735e9bd7a1ad9e768601457755758a4c324d
Jason Ellis <palamedes@rocketmail.com>
2020-10-07 16:10:13 -0400

    App controller update adding therapist and client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # [GET] /
   # Default URL for this project
   def index
+    @therapist = Therapist.first
+    @client = Client.first
   end
 
 end
