89a5d3807195f8e85e61ec37e5c5fc45c6e2f586
Jason Ellis <palamedes@rocketmail.com>
2020-10-07 16:09:50 -0400

    Initial seed update with first 3 tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+therapist = Therapist.create name: "John Q Therapist"
+client = Client.create name: "Little Boy Johnny", therapist: therapist
+
+ProgressNote.create( title: "/sh/ IWP imitative",
+                     note: "/sh/ IWP imitative -++--+++-+ 6/10 60%
+errors: sheep, she, shape", therapist: therapist, client: client )
+
+ProgressNote.create( title: "Expressive/Pragmatic Language",
+                     note: "Expressive/Pragmatic Language - book (the tiny seed)
+STG2: named vocabulary in pictures -visual/verbal/max cues
+sun -+
+fish +
+seed +
+bird - -
+grass +
+snow -
+rain -
+foot -
+flower - -
+tree +
+Total: 5/13=38%", therapist: therapist, client: client )
+
+ProgressNote.create( title: "STG3: labeled vocabulary",
+                     note: "STG3: labeled vocabulary (nouns, verbs) -visual/mod cues:
+nouns: +++++ +++ 8/8
+errors: 0
+verbs: ++ - - - - + 3/7
+errors: swim, kick, catch, throw
+Total: 10/13=77%", therapist: therapist, client: client )
