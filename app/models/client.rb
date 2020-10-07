fb94b58c367cddb78f329277e88d70a852cbdbd6
Jason Ellis <palamedes@rocketmail.com>
2020-10-07 16:56:50 -0400

    A client can now have attempts at vocabulary A client can now have words through attempts sourced to vocabulary

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,9 @@ class Client < ApplicationRecord
   # Relationships
   belongs_to :therapist
   has_many :progress_notes
+  has_many :attempts
+
+  has_many :words, through: :attempts, source: :vocabulary
 
   # Validations
   validates :name, presence: true
