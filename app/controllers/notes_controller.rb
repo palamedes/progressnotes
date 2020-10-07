53bf6d06b24ee51c753726e7ea1f2559060262dd
Jason Ellis <palamedes@rocketmail.com>
2020-10-07 16:34:01 -0400

    Stub of notes controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,28 @@
+class NotesController < ActionController::Base
+
+  layout 'application'
+
+  before_action :set_objects
+
+  # [GET] /
+  # Default URL for notes
+  def index
+  end
+
+  # [GET] /edit
+  def edit
+    @note = @client.progress_notes.find_or_initialize_by id: params[:id]
+  end
+
+  private
+
+  #  Set our objects or redirect
+  def set_objects
+    @therapist = Therapist.find_or_initialize_by id: params[:therapist_id]
+    @client = @therapist.clients.find_or_initialize_by id: params[:client_id]
+    @notes = @client.progress_notes
+
+    redirect_to :root, flash: {error: "Could find those objects."} and return if @therapist.new_record? || @client.new_record?
+  end
+
+end
