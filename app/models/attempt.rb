60884deb5b1e5fb839c1ac863e8b7fa640e77c35
Jason Ellis <palamedes@rocketmail.com>
2020-10-08 10:33:58 -0400

    Words aren't required.  Index of the attempt is.

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -3,8 +3,9 @@ class Attempt < ApplicationRecord
   # Relationships
   belongs_to :vocabulary
   belongs_to :client
+  belongs_to :progress_note
 
   # Validations
-  validates :client, :vocabulary, presence: true
+  validates :client, :note_index, presence: true
 
 end
